85e6947a8992aa33724d92c167f1167304513e08
Jae Hoon Yang <didwogns789@naver.com>
2020-09-04 11:55:58 +0900

    Nest everything in one single root element you return

--- src/App.js
+++ src/App.js
@@ -2,17 +2,18 @@ import React from "react";
 import "./App.css";
 
 function App() {
-  // return (
-  //   <div className="App">
-  //     <h1>Hello World</h1>
-  //   </div>
-  // );
-
-  return React.createElement(
-    "div",
-    { className: "App" },
-    React.createElement("h1", null, "Hi! I'm in App")
+  return (
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+    // <h1>Me too</h1>  => Don't do that
   );
+
+  // return React.createElement(
+  //   "div",
+  //   { className: "App" },
+  //   React.createElement("h1", null, "Hi! I'm in App")
+  // );
 }
 
 export default App;
