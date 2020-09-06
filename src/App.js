47d5f0363920f0b08d40ac5ca414e9e44511b58b
Jae Hoon Yang <didwogns789@naver.com>
2020-09-06 09:05:12 +0900

    Understanding and Using state

--- src/App.js
+++ src/App.js
@@ -1,25 +1,47 @@
-import React from "react";
+import React, { Component } from "react";
 import "./App.css";
 import Person from "./person/Person";
 
-function App() {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-      <p>Paragraph 1</p>
-      <Person name="Hoon" age="22" />
-      <Person name="Crazybirdz" age="23" />
-      <Person name="CrazyEagle" age="24" />
-      <Person name="CrazyCrow" age="25" />
-    </div>
-    // <h1>Me too</h1>  => Don't do that because React should use one root elements
-  );
+class App extends Component {
+  state = {
+    person: [
+      { name: "Hoon", age: 22 },
+      { name: "Crazybirdz", age: 23 },
+      { name: "CrazyEagle", age: 24 },
+      { name: "CrazyCrow", age: 25 },
+    ],
+  };
 
-  // return React.createElement(
-  //   "div",
-  //   { className: "App" },
-  //   React.createElement("h1", null, "Hi! I'm in App")
-  // );
+  render() {
+    return (
+      <div className="App">
+        <h1>Hello World</h1>
+        <p>Paragraph 1</p>
+        <Person name={this.state.person[0].name} age={this.state.person[0].age}>
+          Hobby is traveling to foriegn that have beatiful landscape!
+        </Person>
+        <Person
+          name={this.state.person[1].name}
+          age={this.state.person[1].age}
+        />
+        <Person
+          name={this.state.person[2].name}
+          age={this.state.person[2].age}
+        />
+        <Person
+          name={this.state.person[3].name}
+          age={this.state.person[3].age}
+        />
+      </div>
+      // <h1>Me too</h1>  => Don't do that because React should use one root elements
+    );
+
+    // return React.createElement(
+    //   "div",
+    //   { className: "App" },
+    //   React.createElement("h1", null, "Hi! I'm in App")
+    // );
+  }
 }
 
 export default App;
--- src/person/Person.js
+++ src/person/Person.js
@@ -2,9 +2,12 @@ import React from "react";
 
 const person = (props) => {
   return (
-    <p>
-      I'm {props.name} and I am {props.age} years old.
-    </p>
+    <div>
+      <p>
+        I'm {props.name} and I am {props.age} years old.
+      </p>
+      <p>{props.children}</p>
+    </div>
     // But if you use props in class-based components, you should code like {this.props.name}.
   );
 };
