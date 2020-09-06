926ea1c93c93022c8234d2716e7431c5fa7ebb7a
Jae Hoon Yang <didwogns789@naver.com>
2020-09-06 09:27:46 +0900

    Manipulating the state

--- src/App.js
+++ src/App.js
@@ -4,16 +4,26 @@ import Person from "./person/Person";
 
 class App extends Component {
   state = {
-    person: [
+    persons: [
       { name: "Hoon", age: 22 },
       { name: "Crazybirdz", age: 23 },
       { name: "CrazyEagle", age: 24 },
       { name: "CrazyCrow", age: 25 },
     ],
+    otherState: "The other state",
   };
 
   handleClicked = () => {
-    console.log("Button was clicked!");
+    // Don't Do that: this.state.persons[0].name = "Jae Hoon Yang";
+    this.setState({
+      persons: [
+        { name: "Jae Hoon Yang", age: 24 },
+        { name: "Crazybirdz", age: 23 },
+        { name: "CrazyEagle", age: 24 },
+        { name: "CrazyCrow", age: 25 },
+      ],
+    });
+    console.log(this.state.otherState);
   };
 
   render() {
@@ -22,20 +32,23 @@ class App extends Component {
         <h1>Hello World</h1>
         <button onClick={this.handleClicked}>Change Name</button>
         <p>Paragraph 1</p>
-        <Person name={this.state.person[0].name} age={this.state.person[0].age}>
+        <Person
+          name={this.state.persons[0].name}
+          age={this.state.persons[0].age}
+        >
           Hobby is traveling to foriegn that have beatiful landscape!
         </Person>
         <Person
-          name={this.state.person[1].name}
-          age={this.state.person[1].age}
+          name={this.state.persons[1].name}
+          age={this.state.persons[1].age}
         />
         <Person
-          name={this.state.person[2].name}
-          age={this.state.person[2].age}
+          name={this.state.persons[2].name}
+          age={this.state.persons[2].age}
         />
         <Person
-          name={this.state.person[3].name}
-          age={this.state.person[3].age}
+          name={this.state.persons[3].name}
+          age={this.state.persons[3].age}
         />
       </div>
       // <h1>Me too</h1>  => Don't do that because React should use one root elements
