f2dffbbeb0f067e7e1539c49f31b4e3e5b6dd3d5
Jae Hoon Yang <didwogns789@naver.com>
2020-09-08 20:31:43 +0900

    Using Refs with React Hook(useRef(), useEffect()) in functional component together with useEffect

--- src/components/Cockpit/Cockpit.js
+++ src/components/Cockpit/Cockpit.js
@@ -1,20 +1,23 @@
-import React, { useEffect } from "react";
+import React, { useEffect, useRef } from "react";
 
 import classes from "./Cockpit.module.css";
 
 const Cockpit = (props) => {
+  const toggleBtnRef = useRef(null);
+
   useEffect(() => {
     console.log("[Cockpit.js] useEffect");
     alert("First rendering");
+    toggleBtnRef.current.click();
     return () => console.log("[Cockpit.js] cleanup work in useEffect");
   }, []);
 
   useEffect(() => {
     console.log("[Cockpit.js] 2nd useEffect");
     // HTTP request...
-    setTimeout(() => {
-      alert("Save user to cloud");
-    }, 1000);
+    // setTimeout(() => {
+    //   alert("Save user to cloud");
+    // }, 1000);
     return () => console.log("[Cockpit.js] 2nd cleanup work in useEffect");
   });
 
@@ -37,7 +40,7 @@ const Cockpit = (props) => {
     <div className={classes.Cockpit}>
       <h1>{props.title}</h1>
       <p className={assignClasses.join(" ")}>This is really Working!!</p>
-      <button className={btnClass} onClick={props.clicked}>
+      <button ref={toggleBtnRef} className={btnClass} onClick={props.clicked}>
         Show Users
       </button>
     </div>
