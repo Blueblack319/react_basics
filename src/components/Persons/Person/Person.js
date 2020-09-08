1e9b5cc9554afc3089787bd172421c95db7efb1b
Jae Hoon Yang <didwogns789@naver.com>
2020-09-08 20:17:22 +0900

    Two ways of Using Refs in class-based Component ref is used to access real DOM element!

--- src/components/Persons/Person/Person.js
+++ src/components/Persons/Person/Person.js
@@ -6,6 +6,16 @@ import withClass from "../../../hoc/withClass";
 // import "./Person.css";
 
 class Person extends Component {
+  constructor(props) {
+    super(props);
+    this.inputElementRef = React.createRef();
+  }
+
+  componentDidMount() {
+    // this.inputElement.focus();
+    this.inputElementRef.current.focus();
+  }
+
   render() {
     console.log("[Person.js] rendering...");
     return (
@@ -16,6 +26,8 @@ class Person extends Component {
         <p>{this.props.children}</p>
         <input
           type="text"
+          // ref={(inputEl) => (this.inputElement = inputEl)}
+          ref={this.inputElementRef}
           onChange={this.props.changeName}
           value={this.props.name}
         />
